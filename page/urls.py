d4eeb4992e8d49221d098d88119fe5ede949288d
minsgy <alstjrchl97@naver.com>
2020-08-09 20:06:02 +0900

    order.html - model.money 연결

--- page/urls.py
+++ page/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import cover, order, about
+from .views import cover, order, about, finish
 urlpatterns = [
     path('', cover, name="cover"), # Cover 처음 페이지
     path('order/', order, name="order"), # Order 페이지 - 메뉴주문 메인 페이지
     path('about/', about, name="about"), # About 페이지 - 페이지 설명 페이지
+    path('finish/', finish, name="finish"),
 ]
